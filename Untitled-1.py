5f04b7a4eb32ad269fb288125ce3e20743482b0a
NOG-LX <davimeireles749@gmail.com>
2025-10-02 18:29:46 -0300

    Add files via upload
    This Project is a small calculator, its noting important.

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+import time
+print ("Bem vindo a calculadora!")
+
+time.sleep(3)
+
+print ('digite a equação desejada')
+time.sleep(1)
+int = a = (1,2,3,4,5,6,7,8,9,0)
+int = b = (1,2,3,4,5,6,7,8,9,9)
