8fcbfd0649518e545d4349b20b3a90715265c63f
NOG <davimeireles749@gmail.com>
2026-01-13 20:55:13 -0300

    Update github test-1.py

--- github test-1.py
+++ github test-1.py
@@ -1,12 +1,1 @@
-import time
-print ('Bem vindo a calculadora!')
-time.sleep(2)
-print ('Digite a equação desejada (ex: 2+3 ou 10/2 ou a + (b * c) - n/m))')
-time.sleep(1)
-equacao = input(">>>")
-try:
-    resultado = eval(equacao) #avalia a expressão digitada
-    print("Resultado:", resultado)
-except:
-    print("equação inválida!")
 
